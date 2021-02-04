c63dc877e90ff336ae5afbc5c447c068a17e82d6
Jordan Taylor <jorrtayl@ut.utm.edu>
2021-02-04 07:36:36 -0600

    Added header.

--- CW 4 - PhoneTypeChecker/Program.cs
+++ CW 4 - PhoneTypeChecker/Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿/* Author: Jordan Taylor
+ * Date: 02 / 04 / 2021
+ * Description: Checks what type of phones there are based on different factories.
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
